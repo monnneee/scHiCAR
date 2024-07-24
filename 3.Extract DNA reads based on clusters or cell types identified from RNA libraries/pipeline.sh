7e36fb1bb9136446d2b9d46d4fc68185f03fc191
YueyuanXu <66565200+monnneee@users.noreply.github.com>
2024-07-24 14:59:50 -0400

    Update pipeline.sh

--- 3.Extract DNA reads based on clusters or cell types identified from RNA libraries/pipeline.sh
+++ 3.Extract DNA reads based on clusters or cell types identified from RNA libraries/pipeline.sh
@@ -3,13 +3,16 @@ for i in {sample1,sample2,...,sample20} #modified based on your sample names
 do
 sort -k1b,1 ${i}_dna_barcode|join -j 1 - ATAC-RNA_barcode.dict|awk '{print"'$i'_"$2"\t'$i'_"$1}' OFS='\t' > ${i}_RNA_ATAC.barcode # the 1st column is RNA barcode and 2nd column is matched DNA barcode
 done
-cat sample*_RNA_ATAC.barcode > total_RNA_ATAC.barcode # merge total samples together
+cat sample*_RNA_ATAC.barcode > total_RNA_ATAC.barcode # merge the total samples together that are used in the 'dna_barcode.R' script
+
 ### 2. export DNA barcodes of each cell from the same cluster/cell type for each sample
-Rscript dna_barcode.R # each cluster has an output file named "total_dna_barcode" that includes mixed samples
-for i in {1..20} #all cluster names or  cell types consistent with "cluster_list" of dna_barcode.R
+Rscript dna_barcode.R #export DNA barcodes for each cluster/cell type that includes mixed samples
+for i in {sample1,sample2,...,sample20} #all cluster names or  cell types are consistent with the "cluster_list" in "dna_barcode.R"
 do
+awk '{if($1=="'$i'"){print$2}}' Astro/Astro_barcode.txt > Astro/${i}_Astro_barcode.txt
 cluster$i/total_dna_barcode
 done
+
 ### 3. extract DNA reads based on barcodes
 zcat DNA_fastq/sample_R1.fastq.gz | awk -F " " '{if(NR%4==1){print $1}}' > DNA_fastq.readName #extract all read names from read1 fastq file of DNA library
 ls *_dna_barcode 
