a192671918811a3fca864cb26659388e1aa6c7b8
YueyuanXu <66565200+monnneee@users.noreply.github.com>
2024-07-24 12:54:53 -0400

    Create dna_barcode.r

---
+++ 3.Extract DNA reads based on clusters or cell types identified from RNA libraries/dna_barcode.r
@@ -0,0 +1,18 @@
+### 1. add DNA barcodes to seurat object
+library(Seurat)
+rna_seurat_object<-readRDS("rna_seurat.rds")
+dna_barcode<-read.table("./total_RNA_DNA_barcode.txt",sep='\t',header=F,row.names=1) #1st column is RNA barcode and 2nd column is DNA barcode
+dna_bd<-dna_barcode$V2
+names(dna_bd)<-rownames(dna_barcode)
+rna_seurat_object$temp<-names(rna_seurat_object$orig.ident)
+rna_filtered_object<-subset(rna_seurat_object,temp %in% rownames(dna_barcode)) #filter out cells without matched DNA barcodes
+rna_filtered_object$temp<-NULL
+rna_filtered_object <- AddMetaData(object = rna_filtered_object, metadata = dna_bd,col.name = 'dna_barcode')
+### 2. extract DNA barcodes after identified clusters or annotated cell types:
+cluster<-names(table(rna_filtered_object$cluster)) #get total cluster name
+cluster_list<-as.list(cluster)
+for (i in 1:length(cluster)){
+bd<-subset(rna_filtered_object,cluster %in% cluster_list[[i]])$dna_barcode
+names(bd)<-NULL
+write.table(bd,paste(cluster_list[[i]],"_dna_barcode",sep=""),sep="\t",quote=FALSE,col.names=FALSE,row.names=FALSE)
+} #each cluster has one *_dna_barcode file
