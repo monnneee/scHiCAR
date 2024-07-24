1d4253030bc46d64c322ee91bef6d82be0ae17f2
YueyuanXu <66565200+monnneee@users.noreply.github.com>
2024-07-24 15:39:54 -0400

    Update pipeline.sh

--- 3.Extract DNA reads based on clusters or cell types identified from RNA libraries/pipeline.sh
+++ 3.Extract DNA reads based on clusters or cell types identified from RNA libraries/pipeline.sh
@@ -23,13 +23,23 @@ do
 zcat DNA_fastq/sample${m}_R1.fastq.gz | awk -F " " '{if(NR%4==1){print $1}}' > sample${m}_DNA_readName #extract all read names from read1 fastq file
 for n in {1..25} #all clusters
 do
-python3 match.py -l sample${m}_DNA_readName -s cluster${n}/sample${m}_barcode.txt -o final_readName/cluster${n}_sample${m}_readName
+python3 readName_match.py -l sample${m}_DNA_readName -s cluster${n}/sample${m}_barcode.txt -o final_readName/cluster${n}_sample${m}_readName
 done
 done
 
 ### 4. generate fastq file for each cluster or cell type
-for i in {}
+for i in {1..25} #all clusters
 do
-python3 match.py -l DNA_fastq.readName
--s ${i}_dna_barcode  -o ${i}.readName
+for j in {1..20}  #all samples
+do
+seqtk subseq DNA_fastq/sample${i}_R1.fastq.gz final_readName/cluster${i}_sample${j}_readName >> cluster${i}_R1.fastq
+seqtk subseq DNA_fastq/sample${i}_R2.fastq.gz final_readName/cluster${i}_sample${j}_readName >> cluster${i}_R2.fastq
+done
+pigz -p 8 cluster${i}_R1.fastq
+pigz -p 8 cluster${i}_R2.fastq
+done
 
+### The above cluster${i}_R*.fastq as pseudo-bulk fastq files were used for running nf-core/hicar 
+(https://github.com/nf-core/hicar).
+nextflow pull jianhong/hicar -r dev2rc #dev2rc is the newest version
+nextflow run jianhong/hicar -profile singularity --genome mm10 -r dev2rc --input samplesheet.csv --skip_fastqc --skip_cutadapt --outdir result --skip_interactions --skip_tads --skip_diff_analysis --skip_peak_qc --skip_igv --skip_trackhub --skip_circos --pairtools_parse_version parse2 -resume
